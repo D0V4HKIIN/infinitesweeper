9ec699d9a2a4dd4b83e2b62676bff2b853040145
D0V4HKIIN <jonas.bonnaudet@gmail.com>
2024-01-12 20:22:07 +0100

    dragging to pan

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-use std::ops::Mul;
-
 use bevy::{
     input::mouse::{MouseScrollUnit, MouseWheel},
-    math::vec3,
     prelude::*,
     window::PrimaryWindow,
 };
@@ -16,6 +13,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
         .add_systems(Update, handle_mouse)
+        .init_resource::<MouseDragData>()
         .run();
 }
 
@@ -47,21 +45,67 @@ fn insert_cell(pos: Transform, mut commands: Commands) {
 // finds which cell has been clicked
 fn handle_mouse(
     mouse_button: Res<Input<MouseButton>>,
+    mut drag_data: ResMut<MouseDragData>,
     mut scroll_event: EventReader<MouseWheel>,
     window_q: Query<&Window, With<PrimaryWindow>>,
-    camera_q: Query<(&Camera, &GlobalTransform)>,
-    mut camera_projection: Query<&mut OrthographicProjection, With<Camera>>,
+    mut camera_q: Query<
+        (
+            &mut Camera,
+            &GlobalTransform,
+            &mut Transform,
+            &mut OrthographicProjection,
+        ),
+        Without<Cell>,
+    >, // without cell because cell_q also has a Globaltransform thing idk
     mut cell_q: Query<(&mut Sprite, &Handle<Image>, &GlobalTransform, Entity), With<Cell>>,
 ) {
-    let (camera, camera_transform) = camera_q.single();
+    let (camera, camera_global_transform, mut camera_transform, mut projection) =
+        camera_q.single_mut(); //
+
     let primary_window = window_q.single();
-    let scale_factor = primary_window.scale_factor() as f32;
+    // let scale_factor = primary_window.scale_factor() as f32;
     let mut active_entity = None;
+
+    // dragging
+    if mouse_button.just_pressed(MouseButton::Left) {
+        if let Some(pos) = primary_window.cursor_position() {
+            drag_data.is_dragging = true;
+            drag_data.drag_start = pos;
+            drag_data.camera_start = camera_transform.translation.clone();
+            println!("camera pos at {}", camera_transform.translation.xy());
+        }
+    }
+
+    if mouse_button.just_released(MouseButton::Left) {
+        drag_data.is_dragging = false;
+    }
+
+    if drag_data.is_dragging {
+        if let Some(pos) = primary_window.cursor_position() {
+            let mut mouse_diff: Vec3 = (pos - drag_data.drag_start).extend(0.);
+
+            mouse_diff.x *= -1.;
+
+            mouse_diff *= projection.scale;
+
+            camera_transform.translation = drag_data.camera_start + mouse_diff;
+
+            println!(
+                    "camera start {} and drag start {}, mouse_diff {}, pos {}, computing {}, huh {}",
+                    drag_data.camera_start,
+                    drag_data.drag_start,
+                    mouse_diff,
+                    pos,
+                    drag_data.camera_start + mouse_diff,
+                    Vec3::new(1., 2., 3.) + Vec3::new(1.1, 2.2, 3.3),
+                );
+        }
+    }
 
     // find clicked cell
     if mouse_button.just_released(MouseButton::Left) {
         if let Some(pos) = primary_window.cursor_position() {
-            if let Some(pos) = camera.viewport_to_world_2d(camera_transform, pos) {
+            if let Some(pos) = camera.viewport_to_world_2d(camera_global_transform, pos) {
                 for (sprite, handle, node_transform, entity) in &mut cell_q.iter_mut() {
                     let size = sprite.custom_size.unwrap(); //sprite.rect.unwrap();
 
@@ -69,11 +113,6 @@ fn handle_mouse(
                     let y_min = node_transform.affine().translation.y - size.y / 2.; // + size.min.y;
                     let x_max = node_transform.affine().translation.x + size.x / 2.; // + size.max.x;
                     let y_max = node_transform.affine().translation.y + size.y / 2.; // + size.max.y;
-
-                    println!(
-                        "entity is in rect ({}, {}), ({}, {})",
-                        x_min, y_min, x_max, y_max
-                    );
 
                     if x_min < pos.x && pos.x < x_max && y_min < pos.y && pos.y < y_max {
                         println!("found entity");
@@ -87,14 +126,18 @@ fn handle_mouse(
     // update color of clicked cell
     if let Some((mut sprite, _handle, _node_transform, _entity)) = active_entity {
         println!("active entity: {:#?}", sprite);
-        sprite.color = Color::rgb(0.75, 0.75, 0.25);
+        if sprite.color == SQUARE_COLOR {
+            sprite.color = Color::rgb(0.75, 0.75, 0.25);
+        } else {
+            sprite.color = SQUARE_COLOR;
+        }
     }
 
     // zoom using scrolling
     for event in scroll_event.read() {
         match event.unit {
             MouseScrollUnit::Line => {
-                let mut projection = camera_projection.single_mut();
+                // let mut projection = camera_projection.single_mut();
                 projection.scale = (projection.scale - event.y * SCROLL_SPEED).clamp(0.1, 100.);
             }
             MouseScrollUnit::Pixel => {
@@ -112,3 +155,10 @@ pub struct Cell;
 
 #[derive(Component)]
 pub struct Bomb;
+
+#[derive(Resource, Default)]
+struct MouseDragData {
+    is_dragging: bool,
+    drag_start: Vec2,
+    camera_start: Vec3,
+}
